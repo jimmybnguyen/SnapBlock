cfff0d12b64f1b149a8c4157e005896562478bb4
jimmybnguyen <jimmyn9@uw.edu>
2017-01-28 11:05:15 -0800

    code cleanup

--- Assets/OpenBook.cs
+++ Assets/OpenBook.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-using Kvant;
 
 public class OpenBook : MonoBehaviour {
 
@@ -45,32 +44,14 @@ public class OpenBook : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("obj enter" + other.gameObject.tag);
-        if (other.gameObject.tag == "Wand" && !bookOpen)
-        {
+        Debug.Log("colliding");
             TurnBookCover();
-
-
-        }
-    }
-  
-    public IEnumerator SparayLeaves()
-    {
-        Debug.Log("start spray");
-        Spray sprayScript = leafEmitter.gameObject.GetComponent<Spray>();
-        yield return new WaitForSeconds(1.2f);
-        leafEmitter.gameObject.SetActive(true);
-        yield return new WaitForSeconds(.2f);
-        sprayScript.throttle = 0;
-        yield return new WaitForSeconds(4f);
-        leafEmitter.SetActive(false);
     }
 
     public void TurnBookCover()
     {
         bookCrontrol.Open_Book();
         bookOpen = true;
-        StartCoroutine(SparayLeaves());
         group.gameObject.SetActive(true);
         group2.gameObject.SetActive(true);
        
