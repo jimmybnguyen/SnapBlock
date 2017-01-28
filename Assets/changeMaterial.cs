6ae72aa68bb79c578f38c3cb37a3c1de83b70162
jimmybnguyen <jimmyn9@uw.edu>
2017-01-28 15:41:29 -0800

    add instructions

--- Assets/changeMaterial.cs
+++ Assets/changeMaterial.cs
@@ -1,37 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class changeMaterial : MonoBehaviour
 {
+    public SteamVR_TrackedObject leftController;
+    public SteamVR_TrackedObject rightController;
     public GameObject display;
 
-    int i = 0;
+    int arrayPos = 0;
     public Material[] myMaterials;
 
     // Use this for initialization
     void Start()
     {
-        myMaterials = myMaterials.GetComponent(typeof(Material)) as Material;
-        /*
-        instruction = display.GetComponent(typeof(TextMesh)) as TextMesh;
-        Debug.Log("starting instruction: " + instruction.text);
-        instruction.text = instructions[i];
-        Debug.Log("0: " + instruction.text);
-        */
+
     }
 
-    void OnTriggerEnter(Collider col)
+    void OnTriggerStay(Collider col)
     {
-        /*
-        instruction = display.GetComponent(typeof(TextMesh)) as TextMesh;
-        i += 1;
-        Debug.Log("prev instruction: " + instruction.text);
-        Debug.Log("triggered, i = " + i);
-        instruction.text = instructions[i];
-        Debug.Log(i + ": " + instruction.text);
-        */
-        myMaterials = myMaterials.GetComponent(typeof(Material)) as Material;
+        SteamVR_Controller.Device deviceLeft = SteamVR_Controller.Input((int)leftController.index);
+        SteamVR_Controller.Device deviceRight = SteamVR_Controller.Input((int)rightController.index);
+        if (deviceLeft.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) || deviceRight.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            arrayPos++;
+            arrayPos %= myMaterials.Length;
+            display.GetComponent<Renderer>().material = myMaterials[arrayPos];
+        }
     }
 }
---
+++ Assets/changeMaterialBack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class changeMaterialBack : MonoBehaviour
+{
+    public SteamVR_TrackedObject leftController;
+    public SteamVR_TrackedObject rightController;
+    public GameObject display;
+
+    int arrayPos = 0;
+    public Material[] myMaterials;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        SteamVR_Controller.Device deviceLeft = SteamVR_Controller.Input((int)leftController.index);
+        SteamVR_Controller.Device deviceRight = SteamVR_Controller.Input((int)rightController.index);
+        if (deviceLeft.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) || deviceRight.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            arrayPos--;
+            arrayPos %= myMaterials.Length;
+            display.GetComponent<Renderer>().material = myMaterials[arrayPos];
+        }
+    }
+}
