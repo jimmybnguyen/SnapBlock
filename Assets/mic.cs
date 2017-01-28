5173afc01d26fdf9c8313396925528972ed1b8e0
jimmybnguyen <jimmyn9@uw.edu>
2017-01-28 13:47:23 -0800

    add webcam feed

---
+++ Assets/mic.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        aud.Play();
+    }
+}
